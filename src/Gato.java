a11af17db5c777a2d77f632134b02087bdfa6779
MateoGarzonVargas <148572220+MateoGarzonVargas@users.noreply.github.com>
2023-11-30 12:54:07 +0100

    Inicio del Proyecto Huellas

---
+++ src/Gato.java
@@ -0,0 +1,12 @@
+public class Gato extends Animal{
+
+    private String virusLeucemia;
+
+    public String getVirusLeucemia() {
+        return virusLeucemia;
+    }
+
+    public void setVirusLeucemia(String virusLeucemia) {
+        this.virusLeucemia = virusLeucemia;
+    }
+}
---
+++ src/Perro.java
@@ -0,0 +1,12 @@
+public class Perro extends Animal{
+
+    private String relacionOtros;
+
+    public String getRelacionOtros() {
+        return relacionOtros;
+    }
+
+    public void setRelacionOtros(String relacionOtros) {
+        this.relacionOtros = relacionOtros;
+    }
+}
